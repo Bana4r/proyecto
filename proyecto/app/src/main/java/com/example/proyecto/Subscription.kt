6df301109fd4a8d272a47840ce47f42c3b48cbe1
Ianloking <68720125+Ianloking@users.noreply.github.com>
2025-11-11 17:58:18 -0600

    Modify subscription section in Subscription.kt
    Updated the subscription section in Subscription.kt.

--- proyecto/app/src/main/java/com/example/proyecto/Subscription.kt
+++ proyecto/app/src/main/java/com/example/proyecto/Subscription.kt
@@ -3,7 +3,7 @@ package com.example.proyecto
 import androidx.room.Entity
 import androidx.room.ForeignKey
 import androidx.room.PrimaryKey
-
+// Modificamos el apartado de suscripcion
 @Entity(tableName = "subscriptions",
         foreignKeys = [ForeignKey(entity = User::class,
                                   parentColumns = ["id"],
